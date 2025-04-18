98e77438073a1002823537aeba98ef9384a05f5b
JsD1834 <josedanilosantosserrato04@gmail.com>
2025-04-18 01:54:23 -0500

    UPD footer y header
    - Se actualizo el diseño del footer y el header
    - Se agregaron funcionalidades en el header y la vista inicial del Dashboard

--- components/header/header.js
+++ components/header/header.js
@@ -1,5 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetch("../components/header/header.html")
         .then(response => response.text())
-        .then(data => document.getElementById("header").innerHTML = data);
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
+            updateStyleActiveLink();
+        })
+    
+    function updateStyleActiveLink() {
+        const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+        const currentPath = window.location.pathname.split("/").pop();
+        navLinks.forEach(link => {
+            const linkPath = link.getAttribute("href");
+        
+            if (linkPath === currentPath) {
+                link.classList.add("active");
+            }
+        
+            link.addEventListener("click", function () {
+                navLinks.forEach(l => l.classList.remove("active"));
+                this.classList.add("active");
+            });
+        });
+    };
+   
 });
